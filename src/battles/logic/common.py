69f6a1890576c05487d2a7bc91caf91f9afabeb2
NorthGiK <loh228putin@gmail.com>
2025-11-23 20:58:31 +0000

    почти рабочие способности карт

--- src/battles/logic/common.py
+++ src/battles/logic/common.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Annotated, Any, Collection, Optional, Union, overload
+from typing import Any, Optional, Union, overload
 
 from src import constants
 from src.battles.exceptions import InvalidDeckSizeError
 from src.battles.schemas import SStandardBattleChoice
-from src.cards.models import Ability, AbilityType, Card, Deck, MCard, SubAbility, TargetType
+from src.cards.models import Ability, AbilityType, Card, MCard, SubAbility, TargetType
 from src.logs import dev_configure, get_logger
 from src.utils.decorators import log_func_call
 
@@ -58,94 +58,108 @@ class CommonCardInBattle:
     active_abilities: dict[SubAbility, int]
 
     @log_func_call(log)
-    def _use_for_card(
-        self: 'CommonCardInBattle',
-        abil: SubAbility,
-    ) -> None:
-        previuous_value: int = getattr(self, abil.type.value)
-        setattr(self, abil.type.value, previuous_value + abil.value)
+    def _apply_stat_change(self, ability_type: AbilityType, value: int) -> None:
+        """изменение статистики с валидацией"""
+        if not hasattr(self, ability_type.value):
+            raise AttributeError(
+                f"нет такого атрибута {ability_type.value}\n"
+                f"{__file__}\n"
+                f"class: `{self.__class__}` method: `{self.__class__._apply_stat_change.__name__}`\n"
+            )
 
-
-    @log_func_call(log)
-    @staticmethod
-    def _use_ability_for_card(
-            card: "CommonCardInBattle",
-            abil: SubAbility,
-            own_deck: list["CommonCardInBattle"],
-            opponent_deck: list["CommonCardInBattle"],
-            duration: int,
-            add_to_active: bool = True,
-        ) -> None:
-        def use_for_deck(
-                deck: Collection[CommonCardInBattle],
-                abil: SubAbility,
-                duration: Optional[int] = None,
-            ) -> None:
-            for _card in deck:
-                _card._use_for_card(abil)
-                if duration is not None:
-                    _card._add_ability_to_active(abil, duration)
+        current_value = getattr(self, ability_type.value)
+        new_value = current_value + value
         
-        match abil.target:
+        # валидация что хп не меньше 0
+        if ability_type == AbilityType.hp and new_value < 0:
+            new_value = 0
+            
+        setattr(self, ability_type.value, new_value)
+
+
+    type Card_List = list['CommonCardInBattle']
+    @log_func_call(log)
+    def _get_target_cards(
+        self,
+        target: TargetType,
+        own_deck: Card_List,
+        opponent_deck: Card_List,
+    ) -> Card_List:
+        """Получение карт-целей"""
+        match target:
             case TargetType.ownself:
-                deck = (card,)
-
+                return [self]
+            
             case TargetType.opponent:
-                deck = opponent_deck
-
-            case TargetType.teammates:
-                deck = own_deck
+                return opponent_deck
+            
+            case TargetType.teammates_only:
+                return [card for card in own_deck if card is not self]
+            
+            case TargetType.teammates_and_own:
+                return own_deck
             
             case TargetType.all:
-                deck = opponent_deck + own_deck
-            
+                return own_deck + opponent_deck
+
             case _:
-                raise DontValidTargetType() # TODO:
-        
-        use_for_deck(deck, abil, duration if add_to_active else None)
+                raise InvalidTargetTypeError(f"Invalid target type: {target}")
 
 
-    @log_func_call(log)
-    def _add_ability_to_active(
-        self: 'CommonCardInBattle',
-        abil: SubAbility,
-        duration: int,
-    ) -> None:
-        self.active_abilities[abil] = duration
-
-
-    @log_func_call(log)
-    def add_ability(
+    def _apply_ability_to_targets(
         self,
-        own_deck: list["CommonCardInBattle"],
-        opponent_deck: list["CommonCardInBattle"],
+        ability: SubAbility,
+        own_deck: Card_List,
+        opponent_deck: Card_List,
+        duration: Optional[int],
     ) -> None:
-        duration: int = self.ability.duration
+        """Применение способности к целевым картам"""
+        target_cards = self._get_target_cards(ability.target, own_deck, opponent_deck)
+        
+        for card in target_cards:
+            card._apply_stat_change(ability.type, ability.value)
+            if duration is not None:
+                card._add_ability_to_active(ability, duration)
+    
+
+    def _add_ability_to_active(self, ability: SubAbility, duration: int) -> None:
+        """Добавление способности в активные"""
+        self.active_abilities[ability] = duration
+
+
+    def use_ability(
+        self,
+        own_deck: Card_List,
+        opponent_deck: Card_List,
+    ) -> None:
+        """метод для использования способности"""
+        duration = self.ability.duration
 
         for sub_ability in self.ability.sub_abilities:
-            self._use_ability_for_card(
-                card=self,
-                abil=sub_ability,
-                duration=duration,
+            self._apply_ability_to_targets(
+                ability=sub_ability,
                 own_deck=own_deck,
                 opponent_deck=opponent_deck,
+                duration=duration,
             )
 
 
-    @log_func_call(log)
-    def process_ability(
-        self,
+    def process_abilities(self) -> None:
+        """Обработка активных способностей с удалением"""
+        expired_abilities: list[SubAbility] = []
         
-    ) -> None:
-        active_abilities = self.active_abilities
-        for abil in active_abilities.keys():
-            active_abilities[abil] -= 1
-
-            if active_abilities[abil] == 0:
-                self._use_ability_for_card(
-                    card=self,
-
-                )
+        # Сначала собираем просроченные способности
+        for ability, duration in self.active_abilities.items():
+            new_duration = duration - 1
+            if new_duration <= 0:
+                expired_abilities.append(ability)
+            else:
+                self.active_abilities[ability] = new_duration
+        
+        # Затем применяем обратный эффект и удаляем
+        for ability in expired_abilities:
+            self._apply_stat_change(ability.type, -ability.value)
+            del self.active_abilities[ability]
 
 
     @log_func_call(log)
